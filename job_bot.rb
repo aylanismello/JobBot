b63cba3fbcc3afd6fc9a6b433a52e852d003fa19
willashley23 <willashley23@gmail.com>
2016-10-13 23:12:24 -0700

    Add second search path.

--- job_bot.rb
+++ job_bot.rb
@@ -18,30 +18,50 @@ class JobBot
 		JOB_BATCH_NUM = 2
 		APPLY_ID = "apply-job-button"
 		JOBS_PAGE = "https://www.linkedin.com/jobs/?trk=nav_responsive_sub_nav_jobs"
+    # second jobs page is a direct link to a linkedIn job search for postings containing 'front end' within the last 24 hours.
+    JOBS_PAGE2 = "https://www.linkedin.com/jobs/search?keywords=Front+End&locationId=us%3A84&f_TP=1&orig=FCTD&trk=jobs_jserp_posted_one_day"
 		HOME = "https://linkedin.com"
 
 	def initialize
 		@driver = Selenium::WebDriver.for :chrome
 		filename = "JobBot:".concat(Time.now.to_s[0..15].split(" ").join("&"))
-
 		@file = File.open(filename, 'w') unless TESTING
-
+    puts "Where to? 1 for linkedin, 2 for indeed"
+    @destination = gets.to_i;
 		login
-		@driver.navigate.to JOBS_PAGE
-		JOB_BATCH_NUM.times {show_more_jobs}
-		get_job_links
+    navigate_to_jobs
+    # Edge case needed: postings within the last 24 hours usually have < 4 pages total, hence 3 used here.
+		3.times {
+      show_more_jobs
+		  get_job_links
+  }
 		iterate_jobs
 		@driver.quit
 	end
 
 	def login
-		@driver.navigate.to HOME
-		element = @driver.find_element(:class, 'login-email')
-		element.send_keys EMAIL
-		element = @driver.find_element(:class, 'login-password')
-		element.send_keys PASSWORD
-		element.submit
+    if @destination == 1
+  		@driver.navigate.to HOME
+  		element = @driver.find_element(:class, 'login-email')
+  		element.send_keys EMAIL
+  		element = @driver.find_element(:class, 'login-password')
+  		element.send_keys PASSWORD
+  		element.submit
+    else
+      # Navigate to indeed. 
+      @driver.navigate.to "https://secure.indeed.com/account/login?service=my&hl=en_US&co=US&continue=http%3A%2F%2Fwww.indeed.com%2F"
+      element = @driver.find_element(:id, 'signin_email')
+      element.send_keys EMAIL
+      element = @driver.find_element(:id, 'signin_password')
+      element.send_keys PASSWORD
+      element.submit
+    end
 	end
+
+  def navigate_to_jobs
+    @driver.navigate.to JOBS_PAGE2
+  end
+
 
 	def get_job_links
 		# @driver.navigate.to JOBS_PAGE
@@ -51,15 +71,14 @@ class JobBot
 
 		puts @driver.title
 
-		links = @driver.find_elements(:css,".item	> a").map{|link| link.attribute(:href)}
+		links = @driver.find_elements(:css,".job-title	> a").map{|link| link.attribute(:href)}
 		companies = @driver.find_elements(:css,".job-info-container .col-right h3").map(&:text)
 		titles = @driver.find_elements(:css,".job-info-container .col-right h2").map(&:text)
 		companies = companies.map.with_index {|name, idx| name.concat(": #{titles[idx]}")}
 
-
-		@jobs = {}
+		@jobs = links
 		companies.each.with_index do |company, idx|
-			@jobs[company] = links[idx]
+			idx = links[idx]
 		end
 	end
 
@@ -67,19 +86,10 @@ class JobBot
 		@driver.navigate.to link
 	end
 
-	def job_apply(apply_button, company)
+	def job_apply(apply_button, company=nil)
 		apply_button.click
 		sleep(LOAD_TIME)
-
-		follow_radio_button = @driver.find_element(:name, 'followCompany')
-		follow_radio_button.click
-
-		phone_field = @driver.find_element(:name, 'phone')
-		phone_field.clear
-		phone_field.send_keys PHONE_NUM
-
 		resume_submit_button = @driver.find_element(:id, 'file-browse-input')
-		resume_submit_button.clear
 		resume_submit_button.send_keys(PATH_TO_RESUME)
 		# wait for resume upload
 		sleep(LOAD_TIME)
@@ -87,6 +97,7 @@ class JobBot
 		submit_app_button = @driver.find_element(:id, 'send-application-button')
 
 		unless TESTING
+      sleep 3
 			submit_app_button.click
 			puts "Applied to #{company}"
 			@file.write("#{company}\n")
@@ -97,32 +108,30 @@ class JobBot
 
 	end
 
-	def is_job_acceptable?(apply_button, company)
+	def is_job_acceptable?(apply_button, company = "nil")
 		apply_button.any? && FILTERS.none?{|filter| company.downcase.include?(filter)}
 	end
 
 	def show_more_jobs
 		sleep(LOAD_TIME)
-		expand_button = @driver.find_element(:class, 'expand-button')
+		expand_button = @driver.find_element(:class, 'next-btn')
 		expand_button.click
 		sleep(LOAD_TIME)
 	end
 
 	def iterate_jobs
 		# job_count = 0
-
-		@jobs.each do |company, link|
-			puts "going to #{company}"
+    sleep 6
+		@jobs.each do |link|
+			# puts "going to #{company}"
 			go_to(link)
+      sleep 3
+      name = @driver.find_element(:class, "company").text
 			sleep(LOAD_TIME)
 			apply_button = @driver.find_elements(:id, 'apply-job-button')
-			job_apply(apply_button.first, company) if is_job_acceptable?(apply_button, company)
+			job_apply(apply_button.first, name) if is_job_acceptable?(apply_button)
 			sleep(LOAD_TIME)
 		end
-
-			# job_count += 1
-			# iterate_jobs if job_count > JOB_BATCH_NUM
-
 
 		@file.close unless TESTING
 	end
